a0bdbbc35f284d6f888a2e53259360d2710e9433
giovanafurlan <RM88936@fiap.com.br>
2021-10-14 20:28:12 -0300

    Incluindo a lista e o form do usuário

--- endorfina/src/App.js
+++ endorfina/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Route, Switch } from 'react-router'
+import FormUsuario from './components/formusuario/FormUsuario'
+import ListaUsuario from './components/listausuario/ListaUsuario'
 import Menu from './components/menu/Menu'
 import { Container } from './styled'
 
 export default function App() {
    return(
       <Container>
+
+         <Switch>
+            <Route path="/" exact component={ListaUsuario}></Route>
+            <Route path="/inclui" component={FormUsuario}></Route>
+            <Route path="/edita/:codigo" component={FormUsuario}></Route>
+         </Switch>
+
          <Menu/>
       </Container>
 )}
